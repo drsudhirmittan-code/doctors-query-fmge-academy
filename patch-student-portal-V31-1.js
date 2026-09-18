265c43b9d8904c21cde3e3a96a3de17e12c1ec21
drsudhirmittan-code <drsudhirmittan@gmail.com>
2026-09-18 07:54:36 +0530

    Load cumulative V32-A.3 Change Password visibility patch

--- patch-student-portal-V31-1.js
+++ patch-student-portal-V31-1.js
@@ -16,6 +16,8 @@ if (fs.existsSync(V32_PATCH)) require(V32_PATCH);
 
 const V32_A2_PASSWORD_READY_EMAIL_PATCH = path.join(ROOT, 'patch-student-portal-V32-A.2-Password-Ready-Email.js');
 if (fs.existsSync(V32_A2_PASSWORD_READY_EMAIL_PATCH)) require(V32_A2_PASSWORD_READY_EMAIL_PATCH);
+const V32_A3_CHANGE_PASSWORD_VISIBILITY_PATCH = path.join(ROOT, 'patch-student-portal-V32-A.3-Change-Password-Visibility.js');
+if (fs.existsSync(V32_A3_CHANGE_PASSWORD_VISIBILITY_PATCH)) require(V32_A3_CHANGE_PASSWORD_VISIBILITY_PATCH);
 
 
 const serverFile = path.join(ROOT, 'server.js');
